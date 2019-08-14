dfeef616a32a02c9577974e047837f6e92de963e
Bogdan Koteluk <bogdan.koteluk@gmail.com>
2019-08-14 23:11:28 +0200

    Homework 13.2 changed message when the IOException occur

--- src/pl/homework/app/Calculations.java
+++ src/pl/homework/app/Calculations.java
@@ -34,7 +34,7 @@ public class Calculations {
                 bufferedWriter.write(parameters + " = " + result + "\n");
             }
         }  catch (IOException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
     }
 }
